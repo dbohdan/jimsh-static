a9162b2570f4c1809dbb2d1baf653ecf6d47f5d1
D. Bohdan <dbohdan@dbohdan.com>
2023-04-26 21:46:26 +0000

    feat: build via Dockerfile for caching
    Do not fail the build if Jim Tcl tests fail.

---
+++ Dockerfile
@@ -0,0 +1,56 @@
+ARG arch
+ARG revision
+FROM ${arch}/alpine:3.17
+
+RUN apk update
+RUN apk add gcc \
+            git \
+            hiredis \
+            hiredis-dev \
+            make \
+            libc-dev \
+            openssl-dev \
+            openssl-libs-static \
+            tcl \
+            zlib-dev \
+            zlib-static
+
+RUN wget https://sqlite.org/2023/sqlite-autoconf-3410200.tar.gz
+RUN tar xvf sqlite-autoconf-3410200.tar.gz
+
+RUN cd sqlite-autoconf-3410200/ \
+    && env CFLAGS="-DSQLITE_ENABLE_COLUMN_METADATA \
+                   -DSQLITE_ENABLE_DBSTAT_VTAB \
+                   -DSQLITE_ENABLE_EXPLAIN_COMMENTS \
+                   -DSQLITE_ENABLE_FTS3 \
+                   -DSQLITE_ENABLE_FTS3_PARENTHESIS \
+                   -DSQLITE_ENABLE_FTS3_TOKENIZER \
+                   -DSQLITE_ENABLE_FTS4 \
+                   -DSQLITE_ENABLE_FTS5 \
+                   -DSQLITE_ENABLE_GEOPOLY \
+                   -DSQLITE_ENABLE_JSON1 \
+                   -DSQLITE_ENABLE_MATH_FUNCTIONS \
+                   -DSQLITE_ENABLE_OFFSET_SQL_FUNC \
+                   -DSQLITE_ENABLE_PREUPDATE_HOOK \
+                   -DSQLITE_ENABLE_RBU \
+                   -DSQLITE_ENABLE_RTREE \
+                   -DSQLITE_ENABLE_SESSION \
+                   -DSQLITE_ENABLE_STMTVTAB \
+                   -DSQLITE_ENABLE_UNLOCK_NOTIFY \
+                   -DSQLITE_ENABLE_UPDATE_DELETE_LIMIT \
+                   -DSQLITE_LIKE_DOESNT_MATCH_BLOBS \
+                   -DSQLITE_OMIT_DEPRECATED \
+                   -DSQLITE_SECURE_DELETE \
+                   -DSQLITE_SOUNDEX \
+                  -DSQLITE_USE_ALLOCA \
+                  -DSQLITE_USE_URI" ./configure \
+    && make install
+
+RUN git clone https://github.com/msteveb/jimtcl
+
+RUN cd jimtcl/ \
+    && git checkout ${revision} \
+    && ./configure --full --ipv6 --math --ssl "--with-ext=redis sqlite3" \
+    && make "LDFLAGS=-no-pie -static" \
+            "LDLIBS=-Wl,-Bstatic -lz -lsqlite3 -lssl -lcrypto -lhiredis" \
+    && make test || true
--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
-# Only the target architectures amd64 and i386 have been tested.  The target
+# Only the target architectures amd64 and i386 have been tested. The target
 # architecture is checked to be one of the official architectures in order to
 # guard against typosquatting and name squatting attacks on the Docker Hub.
+
+set -eu
+cd "$(dirname "$0")"
 
 archs="(amd64|arm32v6|arm64v8|i386|ppc64le|s390x)"
 
@@ -12,7 +15,7 @@ usage() {
 
 arch="$1"
 revision="${2:-HEAD}"
-if ! [ -z "$3" ]; then
+if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]; then
     usage
     exit 1
 fi
@@ -29,67 +32,24 @@ elif ! echo "$arch" | grep -E -q "$archs"; then
 fi
 echo "target architecture is $arch"
 
+tag=jimsh-static:$revision-$arch
+printf 'building %s\n' "$tag"
 
 mkdir -p inbox/
+
+docker build \
+    --build-arg arch="$arch" \
+    --build-arg revision="$revision" \
+    --tag "$tag" \
+    . \
+
 docker run \
     --rm \
     --tty \
     --volume "$(pwd)/inbox:/inbox" \
-    "$arch/alpine:3.17" \
+    "$tag" \
     sh -c \
-    'cd \
-     && apk update \
-     && apk add gcc \
-                git \
-                hiredis \
-                hiredis-dev \
-                make \
-                libc-dev \
-                openssl-dev \
-                openssl-libs-static \
-                tcl \
-                zlib-dev \
-                zlib-static \
-     && wget https://sqlite.org/2022/sqlite-autoconf-3400100.tar.gz \
-     && tar xvf sqlite-autoconf-3410200.tar.gz \
-     && cd sqlite-autoconf-3410200/ \
-     && env CFLAGS="-DSQLITE_ENABLE_COLUMN_METADATA \
-                    -DSQLITE_ENABLE_DBSTAT_VTAB \
-                    -DSQLITE_ENABLE_EXPLAIN_COMMENTS \
-                    -DSQLITE_ENABLE_FTS3 \
-                    -DSQLITE_ENABLE_FTS3_PARENTHESIS \
-                    -DSQLITE_ENABLE_FTS3_TOKENIZER \
-                    -DSQLITE_ENABLE_FTS4 \
-                    -DSQLITE_ENABLE_FTS5 \
-                    -DSQLITE_ENABLE_GEOPOLY \
-                    -DSQLITE_ENABLE_JSON1 \
-                    -DSQLITE_ENABLE_MATH_FUNCTIONS \
-                    -DSQLITE_ENABLE_OFFSET_SQL_FUNC \
-                    -DSQLITE_ENABLE_PREUPDATE_HOOK \
-                    -DSQLITE_ENABLE_RBU \
-                    -DSQLITE_ENABLE_RTREE \
-                    -DSQLITE_ENABLE_SESSION \
-                    -DSQLITE_ENABLE_STMTVTAB \
-                    -DSQLITE_ENABLE_UNLOCK_NOTIFY \
-                    -DSQLITE_ENABLE_UPDATE_DELETE_LIMIT \
-                    -DSQLITE_LIKE_DOESNT_MATCH_BLOBS \
-                    -DSQLITE_OMIT_DEPRECATED \
-                    -DSQLITE_SECURE_DELETE \
-                    -DSQLITE_SOUNDEX \
-                    -DSQLITE_USE_ALLOCA \
-                    -DSQLITE_USE_URI" ./configure \
-     && make install \
-     && cd \
-     && git clone https://github.com/msteveb/jimtcl \
-     && cd jimtcl/ \
-     && git checkout "$1" \
-     && ./configure --full --ipv6 --math --ssl "--with-ext=redis sqlite3" \
-     && make "LDFLAGS=-no-pie -static" \
-             "LDLIBS=-Wl,-Bstatic -lz -lsqlite3 -lssl -lcrypto -lhiredis" \
-     && make test \
+    'cd jimtcl \
      && cp jimsh \
-          "/inbox/jimsh-$(./jimsh --version)-$(git rev-parse HEAD \
-                                               | cut -c 1-10)-$0"
-    ' \
-    "$arch" \
-    "$revision"
+           "/inbox/jimsh-$(./jimsh --version)-$(git rev-parse HEAD \
+                    | cut -c 1-10)-$0"' "$arch"
